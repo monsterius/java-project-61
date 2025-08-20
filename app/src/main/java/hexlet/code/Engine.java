1c456978600475bb97c3114a04c653ead079bbd0
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-08-20 15:34:39 +0300

    Add some new variables to Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,11 +3,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Engine {
-    public static Scanner scanner = new Scanner(System.in); // Сканер
+    public static final Scanner scanner = new Scanner(System.in); // Сканер
     public static String name = ""; // Имя пользователя
     public static int counter = 0; // Счетчик успешных решений
     public static boolean gameOver = false; // Индикатор победы/поражения
     public static Random generator = new Random(); // Генератор рандомных чисел
+    public static int result = 0;
+    public static String input = "";
 
     public static void dispGreetAndInit(int variation) { // Приветствие и ввод имени
         counter = 0;
