7e83cde18cc580cec881c722ab43eca54f423414
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-08-20 14:08:39 +0300

    Add dispGreet method in Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,4 +1,13 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class Engine {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String name = "";
+
+    public static void dispGreet() {
+        System.out.println("May I have your name?");
+        name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
 }
