3ebcec113cc04dc14602524a37c3f7537eca9745
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-08-20 14:55:03 +0300

    Modify Engine.dispGreetAndInit and public variables

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 public class Engine {
     public static Scanner scanner = new Scanner(System.in);
     public static String name = "";
-
-    public static void dispGreet() {
-        System.out.println("May I have your name?");
-        name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-    }
+    public static int counter = 0;
+    public static void dispGreetAndInit(int variation) {
+        counter = 0;
+        switch (variation) {
+            case 1 -> { // Even
+                System.out.println("May I have your name?");
+                name = scanner.nextLine();
+                System.out.println("Hello, " + name + "!");
+            }
+            case 2 -> { // Calculator
+                System.out.println("May I have your name?");
+                name = scanner.nextLine();
+                System.out.println("Hello, " + name + "!\nWhat is the result of the expression?");
+            }
+    }   }
 }
