0d6df448a34c59284cb173df7e1574c6e6ea8787
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-08-26 13:22:31 +0300

    Add isPrime method

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -62,4 +62,23 @@ public class Engine {
         }
         return list;
     }
+    // Метод определения простого числа
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        int sqrtN = (int) Math.sqrt(n);
+        for (int i = 3; i <= sqrtN; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
