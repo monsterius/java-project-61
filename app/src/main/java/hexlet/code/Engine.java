a2dc983c4e37a84c0ab818bdb9d35157a1afe372
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-08-28 22:05:49 +0300

    Add basic check methods and variables

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -14,40 +14,26 @@ public class Engine {
     public static String correctInput = "Correct!"; // Удачная попытка
     public static String errA = " is wrong answer ;(. Correct answer was ";
     public static String errB = "\nLet's try again, ";
+    public static String[][] gameData = new String[3][2];
+    public static int currentRound = 0;
     // Метод приветствия и запроса имени
-    public static void startGame(int variation) { // Приветствие и ввод имени
+    public static void startGame(String question) { // Приветствие и ввод имени
         counter = 0;
         var nameReq = "May I have your name?";
         var greet = "Hello, ";
-        switch (variation) {
-            case 1 -> { // Even
-                System.out.println(nameReq);
-                name = scanner.nextLine();
-                System.out.println(greet + name + "!");
-            }
-            case 2 -> { // Calculator
-                System.out.println(nameReq);
-                name = scanner.nextLine();
-                System.out.println(greet + name + "!\nWhat is the result of the expression?");
-            }
-            case 3 -> { // GCD
-                System.out.println(nameReq);
-                name = scanner.nextLine();
-                System.out.println(greet + name + "!\nFind the greatest common divisor of given numbers.");
-            }
-            case 4 -> { // Progression
-                System.out.println(nameReq);
-                name = scanner.nextLine();
-                System.out.println(greet + name + "!\nWhat number is missing in the progression?");
-            }
-            case 5 -> {
-                System.out.println(nameReq);
-                name = scanner.nextLine();
-                System.out.println(greet + name + "!");
-                System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-            }
-            default -> System.out.println(error);
-        }
+        System.out.println(nameReq);
+        name = scanner.nextLine();
+        System.out.println(greet + name + "!\n" + question);
+    }
+    // Метод сравнения результата с правильным ответом
+    public static boolean checkAnswer(String answer) {
+        return answer.equals(gameData[currentRound][1]);
+    }
+    // Метод вывода в случае ошибки
+    public static void wrongAnswer(String answer, String result) {
+        var partA = " is wrong answer ;(. Correct answer was ";
+        var partB =  "\nLet's try again, ";
+        System.out.println(answer + partA + result + partB + "!");
     }
     // Методы возврата инпута String или int в требуемом формате
     public static String encloseStr(String str) {
@@ -55,35 +41,5 @@ public class Engine {
     }
     public static String encloseInt(int integer) {
         return "'" + integer + "'";
-    }
-    // Генератор числовой последовательности
-    public static String[] generateList() {
-        String[] list = new String[9];
-        var firstNumber = generator.nextInt(15);
-        var step = generator.nextInt(6) + 1;
-        list[0] = firstNumber + "";
-        for (int i = 1; i < list.length; i++) {
-            list[i] = Integer.parseInt(list[i - 1]) + step + "";
-        }
-        return list;
-    }
-    // Метод определения простого числа
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        if (n == 2) {
-            return true;
-        }
-        if (n % 2 == 0) {
-            return false;
-        }
-        int sqrtN = (int) Math.sqrt(n);
-        for (int i = 3; i <= sqrtN; i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
