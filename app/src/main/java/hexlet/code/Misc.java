6b81a43ed3895afc33115cf12c75f9cbd996115b
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-08-29 14:14:10 +0300

    Add Misc class

---
+++ app/src/main/java/hexlet/code/Misc.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+public class Misc {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        else if (number == 2) {
+            return true;
+        }
+        else if (number % 2 == 0) {
+            return false;
+        }
+        int sqrtN = (int) Math.sqrt(number);
+        for (int i = 3; i <= sqrtN; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
