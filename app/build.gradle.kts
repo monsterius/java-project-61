47576fae201027dd5ebcf4123f22cc6a2c0dea4e
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-09-01 10:09:47 +0300

    Delete unused code int build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     application
     id("org.sonarqube") version "6.2.0.5505"
     checkstyle
@@ -24,9 +23,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
