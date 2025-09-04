ae504a8618501839d80e9d88ad57e149da37f44a
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-09-04 20:39:43 +0300

    Add generateRandomNumber method to Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,4 +1,12 @@
 package hexlet.code;
 
+import java.util.Random;
+
 public class Utils {
+    private static final Random RANDOM_NUM_GENERATOR = new Random();
+
+    public static int getRandomNumber(int range) {
+        int randomInt = RANDOM_NUM_GENERATOR.nextInt(range) + 1;
+        return randomInt;
+    }
 }
