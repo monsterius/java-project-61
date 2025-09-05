d33a150c99dc5e324157cf3fc616ac9e92663877
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-09-05 15:19:26 +0300

    Delete while loop

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,24 +8,34 @@ import hexlet.code.games.Progression;
 
 public class App {
     public static void main(String[] args) {
-        while (!Engine.getGameOverStatus()) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet\n2 - Even\n3 - Calculator\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
-            Scanner sc = new Scanner(System.in);
-            String game = sc.nextLine();
-            switch (game) {
-                case "1" -> {
-                    System.out.println("Welcome to the Brain Games!");
-                    Cli.welcome();
-                }
-                case "0" -> System.out.println("Goodbye!");
-                case "2" ->  Even.evenGame();
-                case "3" -> Calculator.calculatorGame();
-                case "4" -> Gcd.gCDGame();
-                case "5" -> Progression.progressionGame();
-                case "6" -> Prime.primeGame();
-                default -> System.out.println("Error! " + Engine.encloseStr(game) + " is not a valid game number.");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calculator\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
+        Scanner sc = new Scanner(System.in);
+        String game = sc.nextLine();
+        switch (game) {
+            case "1" -> {
+                System.out.println("Welcome to the Brain Games!");
+                Cli.welcome();
             }
+            case "0" -> {
+                System.out.println("Goodbye!");
+            }
+            case "2" ->  {
+                Even.evenGame();
+            }
+            case "3" -> {
+                Calculator.calculatorGame();
+            }
+            case "4" -> {
+                Gcd.gCDGame();
+            }
+            case "5" -> {
+                Progression.progressionGame();
+            }
+            case "6" -> {
+                Prime.primeGame();
+            }
+            default -> System.out.println("Error! " + Engine.encloseStr(game) + " is not a valid game number.");
         }
     }
 }
