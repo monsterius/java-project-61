3076ec15c793c23351c12655f6cb146927292a8c
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-09-08 10:44:58 +0300

    Make random re-usable

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,9 +3,9 @@ package hexlet.code;
 import java.util.Random;
 
 public class Utils {
+    private static final Random RANDOM_NUM_GENERATOR = new Random();
 
     public static int getRandomNumber(int min, int max) {
-        final Random randomNumGenerator = new Random();
-        return randomNumGenerator.nextInt(max - min + 1) + min;
+        return RANDOM_NUM_GENERATOR.nextInt(max - min + 1) + min;
     }
 }
