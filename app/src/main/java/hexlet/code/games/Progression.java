e5796645d009de57b85cc7a4fd0f50569d5802cf
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-09-08 10:38:01 +0300

    Another progression fix

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -6,17 +6,13 @@ import hexlet.code.Utils;
 import java.util.Arrays;
 
 public class Progression {
-    public static String createProgression(int firstNum, int step, int length) {
+    public static String[] createProgression(int firstNum, int step, int length) {
         String[] array = new String[length];
-        String progression;
-        String result;
         array[0] = firstNum + "";
         for (int i = 1; i < array.length; i++) {
             array[i] = Integer.parseInt(array[i - 1]) + step + "";
         }
-        progression = Arrays.toString(array).replace("[", "").replace("]", "").replace(",", "");
-        result = progression;
-        return result;
+        return array;
     }
     public static void progressionGame() {
         // Передача данных в движок
@@ -29,13 +25,14 @@ public class Progression {
         for (int i = 0; i < Engine.ROUNDS; i++) {
             int step = Utils.getRandomNumber(randomNumMin, stepRange);
             int firstNum = Utils.getRandomNumber(randomNumMin, randomNumMax);
-            String progressionToProcess = createProgression(firstNum, step, progressionLength);
-            String[] arrayProgression = progressionToProcess.split(" ");
-            int index = Utils.getRandomNumber(randomNumMin, progressionLengthIndexMax);
-            String numToLook = arrayProgression[index];
-            String finalProgression = progressionToProcess.replace(numToLook, "..");
+            int hiddenNumIndex = Utils.getRandomNumber(randomNumMin, progressionLengthIndexMax);
+            String[] progressionToProcess = createProgression(firstNum, step, progressionLength);
+            String hiddenNum = progressionToProcess[hiddenNumIndex];
+            progressionToProcess[hiddenNumIndex] = "..";
+            String finalProgression = Arrays.toString(progressionToProcess);
+            finalProgression = finalProgression.replace("[", "").replace("]", "").replace(",", "");
             gameData[i][0] = finalProgression;
-            gameData[i][1] = numToLook;
+            gameData[i][1] = hiddenNum;
         }
         // Начало игры
         var question = "What number is missing in the progression?";
