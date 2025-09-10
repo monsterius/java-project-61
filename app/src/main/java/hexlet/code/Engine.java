d7c6ee9ba3f395a596cc987292e44bd50ec7915d
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-09-10 15:33:14 +0300

    Convert class var's to local ones where it's possible

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class Engine {
     // Общие переменные
     private static final Scanner SCANNER = new Scanner(System.in); // Сканер
-    private static int counter = 0; // Счетчик успешных решений
     private static boolean gameOver = false; // Индикатор победы/поражения
-    private static String name = ""; // Имя пользователя
     public static final int ROUNDS = 3;
     // Начало игры
     public static void startGame(String question, String[][] gameData) { // Приветствие и ввод имени
-        counter = 0;
+        // Счетчик успешных решений
+        int counter = 0;
         var nameReq = "May I have your name?";
         var greet = "Hello, ";
         var partA = " is wrong answer ;(. Correct answer was ";
@@ -18,7 +17,7 @@ public class Engine {
         String correctInput = "Correct!";
 
         System.out.println(nameReq);
-        name = SCANNER.nextLine();
+        String name = SCANNER.nextLine();
         System.out.println(greet + name + "!\n" + question);
         for (String[] data : gameData) {
             System.out.println("Question: " + gameData[counter][0]);
